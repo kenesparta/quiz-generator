2833e2bab496dd594ed52170156067be9b802dc3
Ken Esparta <kenesparta@pm.me>
2025-02-09 22:55:06 -0500

    feat: starting with axum and tokio

--- quizz/src/main.rs
+++ quizz/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use axum::http::StatusCode;
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(health_check));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3003")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}

cb9b7de1bfb3a76bcc1dacb290083850a4fc2de8
Ken Esparta <kenesparta@pm.me>
2025-02-23 20:16:31 -0500

    chore: using swc plugin

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <p>It works!</p>
+      <h1>QUIZZ</h1>
     </>
   )
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,9 @@
 import {defineConfig, loadEnv} from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+
+const config = {
+  VITE_PORT: '3333'
+}
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd())
@@ -8,7 +12,7 @@ export default defineConfig(({mode}) => {
       plugins: [react()],
       server: {
         host: '0.0.0.0',
-        port: Number(env.VITE_PORT),
+        port: Number(env.VITE_PORT || config.VITE_PORT),
       },
     }
   }

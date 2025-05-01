52bed9e90d81a718b28c13ea27df7a29770a9984
Ken Esparta <kenesparta@pm.me>
2025-04-30 20:03:43 -0500

    chore: refactoring subtitle

--- web/src/components/shared/Title/SubTitle.tsx
+++ web/src/components/shared/Title/SubTitle.tsx
@@ -1,7 +1,11 @@
-export const SubTitle = ({ name }) => {
+interface SubTitleProps {
+  name: string;
+}
+
+export const SubTitle = ({ name }: SubTitleProps) => {
   return (
     <>
-      <h2 className="text-lg text-cyan-700 font-bold">{name}</h2>
+      <h2 className={`text-lg text-cyan-700 font-bold`}>{name}</h2>
     </>
   )
 }
--- web/src/components/shared/Title/Title.tsx
+++ web/src/components/shared/Title/Title.tsx
@@ -1,4 +1,8 @@
-export const Title = ({ name }) => {
+interface TitleProps {
+  name: string;
+}
+
+export const Title = ({ name }: TitleProps) => {
   return (
     <>
       <h1 className="text-2xl text-blue-700 font-bold">{name}</h1>

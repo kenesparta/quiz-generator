17fc70e737024dc76b8ba1d1634f2889daf9e873
Ken Esparta <kenesparta@pm.me>
2025-05-26 10:25:38 -0500

    chore: removing unecesary code

--- cmd/api/src/controller/examen/dto.rs
+++ cmd/api/src/controller/examen/dto.rs
@@ -2,8 +2,6 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Clone)]
 pub struct RegistrarExamenDTO {
-    pub id: String,
     pub titulo: String,
     pub descripcion: String,
-    pub activo: bool,
 }

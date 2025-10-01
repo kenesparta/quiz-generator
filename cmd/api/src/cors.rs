5a3930dc92b2e03c01bf00d1652573bfc5d71224
Ken Esparta <kenesparta@pm.me>
2025-09-30 21:25:43 -0500

    feat: update CORS

--- cmd/api/src/cors.rs
+++ cmd/api/src/cors.rs
@@ -2,10 +2,10 @@ use actix_cors::Cors;
 
 pub fn set_cors() -> Cors {
     Cors::default()
-        .allowed_origin("http://127.0.0.1:5173/")
-        .allowed_origin("http://127.0.0.1:5173")
-        .allowed_origin("http://localhost:5173/")
-        .allowed_origin("http://localhost:5173")
+        .allowed_origin("http://127.0.0.1:3000/")
+        .allowed_origin("http://127.0.0.1:3000")
+        .allowed_origin("http://localhost:3000/")
+        .allowed_origin("http://localhost:3000")
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
         .allowed_headers(vec![
             http::header::AUTHORIZATION,
